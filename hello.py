f5e07add7b1212f102ef3b9c1e5cd2f0736f7d62
bhagyashree <kalnoorbhagyashree@gmail.com>
2025-11-27 12:05:27 +0530

    changes  added

--- hello.py
+++ hello.py
@@ -1,63 +1,137 @@
-print("hello world")
-a=12
-b=78
-c=4
-temp=a
-a=b
-b=c
-c=temp
-print(a)
-print(b)
-print(c)
-name="BHAGYASHREE"
+# print("hello world")
+# a=12
+# b=78
+# c=4
+# temp=a
+# a=b
+# b=c
+# c=temp
+# print(a)
+# print(b)
+# print(c)
+# name="BHAGYASHREE"
+# # print(name)
+# a=input("enter the value:")
+# print(a)
+# b="5"
+# print(b)
+# print(type(a))
+# print(type(b))
+# a=int(input("enter the value"))
+# print(a)
+# b=float(input("enter the value"))
+# print(b)
+# print(type(a))
+# print(type(b))
+# c=complex (input("enter the value"))
+# print(c)
+# print(type(c))
+# name=("bhagya","patil",35,67)
 # print(name)
-a=input("enter the value:")
-print(a)
-b="5"
-print(b)
-print(type(a))
-print(type(b))
-a=int(input("enter the value"))
-print(a)
-b=float(input("enter the value"))
-print(b)
-print(type(a))
-print(type(b))
-c=complex (input("enter the value"))
-print(c)
-print(type(c))
-name=("bhagya","patil",35,67)
-print(name)
-ages=[56,367,8,98]
-print(ages)
-print(type(name))
-print(type(ages))
-books={
-    'bookname':'maxin',
-    'published':'1997'
-}
-print(books)
-print(type(books))
-a=int(input("enter the value:"))
-if a>=18:
- print("yoyu sre eligible to vote")
-b=int(input("enter the vslue:"))
-if b%2==0:
-    print("the number is even")
-else:
-    print("the number is odd")
-a=int(input("enter the value:"))
-if a>=90:
-    print("first class")
-elif a>=80:
-    print("second class")
-else:
-    print("third class")
-name=str(input("enter the name:"))
-age=int(input("|enter the age:"))
-if name=="_":
-    print("you are not entered the name,please re enter the name")
-elif age>=18:
-    print("you are eligible to booking the ticket")
-else:
-    print("not eligible")
+# ages=[56,367,8,98]
+# print(ages)
+# print(type(name))
+# print(type(ages))
+# books={
+#     'bookname':'maxin',
+#     'published':'1997'
+# }
+# print(books)
+# print(type(books))
+# a=int(input("enter the value:"))
+# if a>=18:
+#  print("yoyu sre eligible to vote")
+# b=int(input("enter the vslue:"))
+# if b%2==0:
+#     print("the number is even")
+# else:
+#     print("the number is odd")
+# a=int(input("enter the value:"))
+# if a>=90:
+#     print("first class")
+# elif a>=80:
+#     print("second class")
+# else:
+#     print("third class")
+# name=str(input("enter the name:"))
+# age=int(input("|enter the age:"))
+# if name=="_":
+#     print("you are not entered the name,please re enter the name")
+# elif age>=18:
+#     print("you are eligible to booking the ticket")
+# else:
+#     print("not eligible")
+# syntax
+# def function_name:
+# functionality 
+# a=23
+# b=45
+# def add():
+#     c=a+b
+#     print(c)
+# # add()
+# a=int(input("enter the value a:"))
+# b=int(input("enter the value b:"))
+
+# if a=="" and b=="":
+#     {
+#         print("a and b values are invalid")
+#     }
+# else:
+#     def add():
+#         c=a+b
+#         print(c)
+# # add()
+# def add():
+#     a = input("enter the value a:")
+#     b = input("enter the value of b:")
+#     if a == "" and b == "":
+#         print("a and b are invalid")
+#     else:
+#         c=a+b
+#         print("sum is:",c)
+# add()
+# std1 = float(input("enter the value in %:"))
+# std2 = float(input("enter the value in %:"))
+# std3 = float(input('enter the value in %:'))
+# if std1>std2:
+#     print("std1 having higher percentage")
+# else:
+#     att2 = float(input("enter the attd in %:"))
+#     att3 = float(input("enter the attd in %:"))
+#     if att2>att3:
+#         print("std2 have higher attendance")
+#     else:
+#         print("std3 not get certified")
+# def vote(a,b,c):
+#     a = int(input("enter the age of person1:"))
+#     b = int(input("enter the age of person2:"))
+#     c = int(input("enter the age of person3:"))
+#     if a>90:
+#         print(" senior citizen ,eligible to vote")
+#     elif b>18 and b>=18:
+#         print("eligible")
+#     elif c<18:
+#         print("not eligible")
+#     else:
+#         print("none  of the above")
+# vote(23,67,8)
+# vote(95,32,56)
+# vote(2,6,18)
+# def eligibility():
+#     a=int(input("enter the age:"))
+#     if a>=18:
+#         print("eligible")
+#     else:
+#         print("not eligible")
+# eligibility()
+# eligibility()
+# eligibility()
+# name = [45,78,90,"bhagya","annarao","patil"]
+# for i in name:
+#     if i == "patil":
+#         print("yes the name inn the list")
+#     else:
+#         print("not in the list")
+str = "hello world"
+print(str[::-1])
